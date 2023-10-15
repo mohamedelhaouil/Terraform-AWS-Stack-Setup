c9e1f94fe5a9f67cf7c9c5f80ab567091b309f66
mohamed <wydad.elhaouil@gmail.com>
2023-10-15 07:36:26 +0000

    Add security groups

---
+++ SecurityGroups.tf
@@ -0,0 +1,84 @@
+resource "aws_security_group" "AWS_Stack_beanstalk_elb_sg" {
+  name        = "AWS_Stack_beanstalk_elb_sg"
+  description = "Security group for beanstalk elb"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "AWS_Stack_bastionhost_sg" {
+  name        = "AWS_Stack_bastionhost_sg"
+  description = "Security group for bastion host"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = [var.MY_IP]
+  }
+}
+
+resource "aws_security_group" "AWS_Stack_ec2instance_sg" {
+  name        = "AWS_Stack_ec2instance_sg"
+  description = "Security group for beanstalck ec2 instances"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = [aws_security_group.AWS_Stack_bastionhost_sg.id]
+  }
+}
+
+resource "aws_security_group" "AWS_Stack_backend_sg" {
+  name        = "AWS_Stack_backend_sg"
+  description = "Security group for backend services (RDS, Active mq, elastic cache)"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = [aws_security_group.AWS_Stack_ec2instance_sg]
+  }
+}
+
+resource "aws_security_group_rule" "allow_backend_sg_itself" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.AWS_Stack_backend_sg.id
+  source_security_group_id = aws_security_group.AWS_Stack_backend_sg.id
+}
--- Variables.tf
+++ Variables.tf
@@ -15,16 +15,23 @@ variable "Zones" {
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
+variable "MY_IP" {
+  default = "1.1.1.1/32"
+}
+
 variable "VpcNAME" {
   default = "AWS-Stack-VPC"
 }
+
 variable "VpcCIDR" {
   default = "172.21.0.0/16"
 }
+
 variable "PubSubnetCIDR" {
   type    = list(string)
   default = ["172.21.1.0/24", "172.21.2.0/24", "172.21.3.0/24"]
 }
+
 variable "PrivSubnetCIDR" {
   type    = list(string)
   default = ["172.21.4.0/24", "172.21.5.0/24", "172.21.6.0/24"]
